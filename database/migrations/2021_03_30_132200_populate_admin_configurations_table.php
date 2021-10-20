c582e932f3f9faf5d0ddb70f7bf4c8dd19babb29
moik.o <sashamoik2016@gmail.com>
2021-10-20 19:05:22 +0300

    module changes - lang controllers - middlewares - moved traits - other updates and improvements

--- database/migrations/2021_03_30_132200_populate_admin_configurations_table.php
+++ database/migrations/2021_03_30_132200_populate_admin_configurations_table.php
@@ -17,46 +17,46 @@ class PopulateAdminConfigurationsTable extends Migration
 
         $data = [
             'home' => [
-                AdminConfiguration::imageItem('image', 'preview', 0, '/img/home_img.png'),
-                AdminConfiguration::titleItem('title', 'main title', 1, [
+                AdminConfiguration::itemImage('image', 'preview', 0, '/img/home_img.png'),
+                AdminConfiguration::itemTitle('title', 'main title', 1, [
                     'uk' => 'Назва',
                     'ru' => 'Название',
                     'en' => 'Named',
                 ]),
-                AdminConfiguration::textItem('address', 'address', 1, [
+                AdminConfiguration::itemText('address', 'address', 1, [
                     'uk' => $faker->paragraph(2),
                     'ru' => $faker->paragraph(2),
                     'en' => $faker->paragraph(2),
                 ]),
             ],
             'home_banner' => [
-                AdminConfiguration::titleItem('banner_title', 'text 1', 1, $faker->words(3, true)),
-                AdminConfiguration::imageItem('banner_image', 'image 1', 0, '/img/banner1.png'),
+                AdminConfiguration::itemTitle('banner_title', 'text 1', 1, $faker->words(3, true)),
+                AdminConfiguration::itemImage('banner_image', 'image 1', 0, '/img/banner1.png'),
 
-                AdminConfiguration::titleItem('banner_title', 'text 2', 1, $faker->words(3, true)),
-                AdminConfiguration::imageItem('banner_image', 'image 2', 0, '/img/banner2.png'),
+                AdminConfiguration::itemTitle('banner_title', 'text 2', 1, $faker->words(3, true)),
+                AdminConfiguration::itemImage('banner_image', 'image 2', 0, '/img/banner2.png'),
 
-                AdminConfiguration::titleItem('banner_title', 'text 3', 1, $faker->words(3, true)),
-                AdminConfiguration::imageItem('banner_image', 'image 3', 0, '/img/banner3.png'),
+                AdminConfiguration::itemTitle('banner_title', 'text 3', 1, $faker->words(3, true)),
+                AdminConfiguration::itemImage('banner_image', 'image 3', 0, '/img/banner3.png'),
             ],
             'contacts' => [
-                AdminConfiguration::titleItem('email', 'company.mail@email.com', 0, 'company.mail@email.com'),
-                AdminConfiguration::titleItem('phone', '+38 (0382) 878787', 0, '+38 (0382) 878787'),
-                AdminConfiguration::titleItem('phone', '+38 (053) 4578679', 0, '+38 (053) 4578679'),
-                AdminConfiguration::titleItem('address', 'address', 1, [
+                AdminConfiguration::itemTitle('email', 'company.mail@email.com', 0, 'company.mail@email.com'),
+                AdminConfiguration::itemTitle('phone', '+38 (0382) 878787', 0, '+38 (0382) 878787'),
+                AdminConfiguration::itemTitle('phone', '+38 (053) 4578679', 0, '+38 (053) 4578679'),
+                AdminConfiguration::itemTitle('address', 'address', 1, [
                     'uk' => "вул.Шевченка 1/1A 01000 м. Київ, Україна",
                     'ru' => "ул.Шевченка 1/1A 01000  Киев, Украина",
                     'en' => "Shewchnka Street 1/1A 01000  Kiev, Ukraine",
                 ]),
-                AdminConfiguration::titleItem('map_link', 'Google Map URL', 0, 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d325515.6816222933!2d30.25250901759096!3d50.40213675479182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cf4ee15a4505%3A0x764931d2170146fe!2sKyiv%2C%2002000!5e0!3m2!1sen!2sua!4v1632052962910!5m2!1sen!2sua'),
+                AdminConfiguration::itemTitle('map_link', 'Google Map URL', 0, 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d325515.6816222933!2d30.25250901759096!3d50.40213675479182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cf4ee15a4505%3A0x764931d2170146fe!2sKyiv%2C%2002000!5e0!3m2!1sen!2sua!4v1632052962910!5m2!1sen!2sua'),
 
-                AdminConfiguration::titleItem('instagram', 'Insta', 0, 'https://instagram.com'),
-                AdminConfiguration::titleItem('facebook', 'FB', 0, 'https://facebook.com'),
+                AdminConfiguration::itemTitle('instagram', 'Insta', 0, 'https://instagram.com'),
+                AdminConfiguration::itemTitle('facebook', 'FB', 0, 'https://facebook.com'),
             ],
             'configurations' => [
-                AdminConfiguration::titleItem('show_admin_header', 'Відображення навігації адміністратора на сайті',
+                AdminConfiguration::itemTitle('show_admin_header', 'Відображення навігації адміністратора на сайті',
                     0, 'Дане поле відповідає за відображення навігації для адміністратора'),
-                AdminConfiguration::titleItem('show_debug_footer', 'Відображення інструментів для відлагодження коду',
+                AdminConfiguration::itemTitle('show_debug_footer', 'Відображення інструментів для відлагодження коду',
                     0, 'Дане поле відповідає за відображення спеціального елементу в нижній частині екрану'),
             ],
         ];
--- src/Models/AdminConfiguration.php
+++ src/Models/AdminConfiguration.php
@@ -2,17 +2,26 @@
 
 namespace HexideDigital\AdminConfigurations\Models;
 
-use HexideDigital\HexideAdmin\Traits\Models\PositionSortTrait;
-use HexideDigital\HexideAdmin\Traits\Models\VisibleTrait;
-use HexideDigital\HexideAdmin\Traits\Models\WithTranslationsTrait;
+use Arr;
+use HexideDigital\HexideAdmin\Contracts\WithTypesContract;
+use HexideDigital\HexideAdmin\Models\Traits\PositionSortTrait;
+use HexideDigital\HexideAdmin\Models\Traits\VisibleTrait;
+use HexideDigital\HexideAdmin\Models\Traits\WithTranslationsTrait;
 use Astrotomic\Translatable\Translatable;
+use HexideDigital\HexideAdmin\Models\Traits\WithTypes;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * App\Models\AdminConfiguration
  *
+ * @method static array itemText(string $key, string $name, ?bool $translatable = null, $value = null)
+ * @method static array itemTitle(string $key, string $name, ?bool $translatable = null, $value = null)
+ * @method static array itemImage(string $key, string $name, ?bool $translatable = null, $value = null)
+ *
  * @mixin \HexideDigital\AdminConfigurations\Models\AdminConfigurationTranslation
+ * @mixin \Eloquent
  * @property int $id
  * @property string $key
  * @property string $type
@@ -28,40 +37,40 @@ use Illuminate\Database\Eloquent\Model;
  * @property-read \HexideDigital\AdminConfigurations\Models\AdminConfigurationTranslation|null $translation
  * @property-read Collection|\HexideDigital\AdminConfigurations\Models\AdminConfigurationTranslation[] $translations
  * @property-read int|null $translations_count
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration joinTranslations(?string $modelTable = null, ?string $translationsTable = null, ?string $modelTableKey = null, ?string $translationsTableKey = null)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration listsTranslations(string $translationField)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration notTranslatedIn(?string $locale = null)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration orWhereTranslation(string $translationField, $value, ?string $locale = null)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration orWhereTranslationLike(string $translationField, $value, ?string $locale = null)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration orderByTranslation(string $translationField, string $sortMethod = 'asc')
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration query()
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration sorted(string $direction = 'ASC', string $field = 'position')
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration translated()
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration translatedIn(?string $locale = null)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration visible()
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereGroup($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereInGroupPosition($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereKey($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereStatus($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereTranslatable($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereTranslation(string $translationField, $value, ?string $locale = null, string $method = 'whereHas', string $operator = '=')
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereTranslationLike(string $translationField, $value, ?string $locale = null)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration whereValue($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration withTranslation()
- * @method static \Illuminate\Database\Eloquent\Builder|AdminConfiguration withTranslations()
+ * @method static Builder|self joinTranslations(?string $modelTable = null, ?string $translationsTable = null, ?string $modelTableKey = null, ?string $translationsTableKey = null)
+ * @method static Builder|self listsTranslations(string $translationField)
+ * @method static Builder|self newModelQuery()
+ * @method static Builder|self newQuery()
+ * @method static Builder|self notTranslatedIn(?string $locale = null)
+ * @method static Builder|self orWhereTranslation(string $translationField, $value, ?string $locale = null)
+ * @method static Builder|self orWhereTranslationLike(string $translationField, $value, ?string $locale = null)
+ * @method static Builder|self orderByTranslation(string $translationField, string $sortMethod = 'asc')
+ * @method static Builder|self query()
+ * @method static Builder|self sorted(string $direction = 'ASC', string $field = 'position')
+ * @method static Builder|self translated()
+ * @method static Builder|self translatedIn(?string $locale = null)
+ * @method static Builder|self visible()
+ * @method static Builder|self whereCreatedAt($value)
+ * @method static Builder|self whereDescription($value)
+ * @method static Builder|self whereGroup($value)
+ * @method static Builder|self whereId($value)
+ * @method static Builder|self whereInGroupPosition($value)
+ * @method static Builder|self whereKey($value)
+ * @method static Builder|self whereName($value)
+ * @method static Builder|self whereStatus($value)
+ * @method static Builder|self whereTranslatable($value)
+ * @method static Builder|self whereTranslation(string $translationField, $value, ?string $locale = null, string $method = 'whereHas', string $operator = '=')
+ * @method static Builder|self whereTranslationLike(string $translationField, $value, ?string $locale = null)
+ * @method static Builder|self whereType($value)
+ * @method static Builder|self whereUpdatedAt($value)
+ * @method static Builder|self whereValue($value)
+ * @method static Builder|self withTranslation()
+ * @method static Builder|self withTranslations()
  */
-class AdminConfiguration extends Model
+class AdminConfiguration extends Model implements WithTypesContract
 {
     use Translatable, WithTranslationsTrait;
-    use VisibleTrait, PositionSortTrait;
+    use VisibleTrait, PositionSortTrait, WithTypes;
 
     protected array $translatedAttributes = [
         'content'
@@ -79,20 +88,57 @@ class AdminConfiguration extends Model
         'status',
     ];
 
-
-    public const type_TITLE = 'title'; // short text /input - type=text
-    public const type_TEXT  = 'text';  // long text /textarea
-    public const type_IMAGE = 'image'; // image /input - file select
-
-    public const types = [
-        self::type_TITLE,
-        self::type_TEXT,
-        self::type_IMAGE,
+    protected static array $types = [
+        self::type_TITLE => self::type_TITLE,
+        self::type_TEXT => self::type_TEXT,
+        self::type_IMAGE => self::type_IMAGE,
     ];
 
-    public static function getTypes(): array
+    public const type_TITLE = 'title'; // short text /input - type=text
+    public const type_TEXT = 'text';  // long text /textarea
+    public const type_IMAGE = 'image'; // image /input - file select
+
+    /**
+     * @return mixed|string
+     */
+    public function getValue()
     {
-        return self::types;
+        return $this->translatable
+            ? ($this->type === self::type_IMAGE
+                ? asset($this->translate(app()->getLocale())->content ?? '')
+                : $this->translate(app()->getLocale())->content ?? ''
+            )
+            : $this->value ?? '';
+    }
+
+    /**
+     * @param string|array $name
+     * @return array
+     */
+    public static function var_groups($name = []): array
+    {
+        if (!is_array($name)) {
+            $name = array($name);
+        }
+
+        $data = AdminConfiguration::joinTranslations()->visible()
+            ->whereIn('group', $name)
+            ->orderBy('in_group_position')
+            ->get();
+
+        return self::makeVariablesMap($data);
+    }
+
+    public static function makeVariablesMap(Collection $array): array
+    {
+        $data = [];
+
+        /** @var AdminConfiguration $admin_configuration */
+        foreach ($array as $admin_configuration) {
+            $data[$admin_configuration->group][$admin_configuration->key][] = $admin_configuration->getValue();
+        }
+
+        return $data;
     }
 
     public static function item(string $type, string $key, string $name, ?bool $translatable = null, $value = null): array
@@ -105,70 +151,38 @@ class AdminConfiguration extends Model
 
         if (!empty($translatable)) {
             $item['translatable'] = true;
-            if(is_array($value)) {
-                foreach (config('app.locales') as $locale) {
+            if (is_array($value)) {
+                foreach (config('translatable.locales') as $locale) {
                     $item[$locale] = ['content' => $value[$locale] ?? ''];
                 }
-            }else{
-                foreach (config('app.locales') as $locale){
-                    $item[$locale] = ['content' => $value??''];
+            } else {
+                foreach (config('translatable.locales') as $locale) {
+                    $item[$locale] = ['content' => $value ?? ''];
                 }
             }
-        }else if(!empty($value)){
+        } else if (!empty($value)) {
             $item['value'] = $value;
         }
 
         return $item;
     }
 
-    public static function textItem(string $key, string $name, ?bool $translatable = null, $value = null): array
+    public static function __callStatic($method, $parameters)
     {
-        return self::item(self::type_TEXT, $key, $name, $translatable, $value);
-    }
-    public static function titleItem(string $key, string $name, ?bool $translatable = null, $value = null): array
-    {
-        return self::item(self::type_TITLE, $key, $name, $translatable, $value);
-    }
-    public static function imageItem(string $key, string $name, ?bool $translatable = null, $value = null): array
-    {
-        return self::item(self::type_IMAGE, $key, $name, $translatable, $value);
-    }
-
-
-    /**
-     * @param string|array $name
-     * @return array
-     */
-    public static function var_groups($name = []): array
-    {
-        if (!is_array($name)) {
-            $name = array($name);
+        if(\Str::startsWith($method, 'item')){
+            $type = strtolower(str_replace('item', '', $method));
+            if(in_array($type, self::getTypesKeys())){
+                return self::item(
+                    $type,
+                    Arr::get($parameters, 0),
+                    Arr::get($parameters, 1),
+                    Arr::get($parameters, 2),
+                    Arr::get($parameters, 3)
+                );
+            }
         }
 
-        $data = AdminConfiguration::visible()
-            ->whereIn('group', $name)
-            ->orderBy('in_group_position')
-            ->get();
-
-        return self::makeVariablesMap($data);
+        return parent::__callStatic($method, $parameters);
     }
-
-    public static function makeVariablesMap(Collection $array): array
-    {
-        $data = [];
-        /** @var AdminConfiguration $admin_configuration */
-        foreach ($array as $admin_configuration) {
-            $value = $admin_configuration->translatable
-                ? ($admin_configuration->type === self::type_IMAGE
-                    ? asset($admin_configuration->translate(app()->getLocale())->content??'')
-                    : $admin_configuration->translate(app()->getLocale())->content ?? ''
-                )
-                : $admin_configuration->value ?? '';
-
-            $data[$admin_configuration->group][$admin_configuration->key][] = $value;
-        }
-        return $data;
-    }
-
 
 }
